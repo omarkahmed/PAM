26f96ed706f60e1b96e36829adb05d6de7703d77
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-10-27 10:16:39 -0700

    ORTCE config fixup

--- standalone/machines/ortce/pvc-1t.sh
+++ standalone/machines/ortce/pvc-1t.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 module purge
 module load cmake
 module load intel/oneapi/nightly
-module laod intel-comp-rt
-module load intel/mpich
-module load intel_compute_runtime/release/agama-devel-647
+module load intel-comp-rt
+module load -f intel/mpich
+module load intel/mpi-utils
 
 export YAKL_ARCH=SYCL
 unset CXXFLAGS
@@ -13,9 +15,9 @@ unset FFLAGS
 unset F77FLAGS
 unset F90FLAGS
 
-export CC=mpicc
-export CXX=mpic++
-export FC=mpif90
+export CC=mpiicx
+export CXX=mpiicpx
+export FC=mpiifx
 
 export GATOR_DISABLE=0
 export ZE_AFFINITY_MASK=0.0
@@ -24,10 +26,10 @@ export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
 export YAKL_SYCL_FLAGS="-DYAKL_PROFILE -DHAVE_MPI -O3 -fsycl -sycl-std=2020 -fsycl-unnamed-lambda -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device 12.60.7 -options -ze-opt-large-register-file\" -mlong-double-64 -Xclang -mlong-double-64 -I`nc-config --includedir`"
 export YAKL_F90_FLAGS="-O2 -DSCREAM_DOUBLE_PRECISION"
 export PAM_LINK_FLAGS="-L`nc-config --libdir` -lnetcdf"
-export SCREAM_HOME="$HOME/scream"
-export YAKL_HOME="$HOME/YAKL"
+export SCREAM_HOME="${SCRIPT_DIR}/../../../../../../../../../../"
+export YAKL_HOME="${SCREAM_HOME}/externals/YAKL"
 export PAM_SCREAM_CXX_FLAGS="-O3;-DHAVE_MPI;-I$YAKL_HOME/external"
 export SCREAM_Fortran_FLAGS="-O3"
-export SCREAM_CXX_LIBS_DIR="`pwd`/../../mmf_simplified/build_p3_shoc_cxx"
+export SCREAM_CXX_LIBS_DIR="${SCRIPT_DIR}/../../mmf_simplified/build_p3_shoc_cxx"
 export PAM_NLEV=50
 export PAM_SCREAM_USE_CXX="ON"
